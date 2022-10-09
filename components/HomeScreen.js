4c3de65d48f79475e266d1bcd9c21035d0d05095
Isaak Haas <v-ialdape@expediagroup.com>
2022-10-09 00:40:50 -0500

    adding example

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -5,10 +5,32 @@ export const Home = ({navigation}) => {
     <>
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <Text>Aqui abajo se va a renderizar LotsOfGreetings</Text>
+        <LotsOfGreetings />
       </View>
     </>
   );
 };
+
+// Creamos el componente Greeting, el cual recibe de prop solo name
+const Greeting = ({ name }) => {
+  return (
+    <View style={{alignItems: 'center'}}>
+      <Text>Hello {name}!</Text>
+    </View>
+  );
+}
+// Este es el componente padre de greeting, para poder renderizarlo debes llamarlo en el componente HomeScreen
+// en este caso el export no es necesario porque esta dentro del mismo archivo, pero lo puedes dejar
+export default LotsOfGreetings = () => {
+  return (
+    <View style={{alignItems: 'center', top: 50}}>
+      <Greeting name='Rexxar' />
+      <Greeting name='Jaina' />
+      <Greeting name='Valeera' />
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
