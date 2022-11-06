1f7f0dd954325879126b14b2f4f0ec9a0b1a168a
Isaak Haas <v-ialdape@expediagroup.com>
2022-11-05 23:50:29 -0600

    contador de items en carrito

--- components/CarritoIcono.js
+++ components/CarritoIcono.js
@@ -1,14 +1,46 @@
 import Icon from '@expo/vector-icons/MaterialCommunityIcons';
-
+import React from 'react';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
+import { Context } from '../Context';
 
 export const CarritoIcono = (navigation) => {
-return (
-  <Icon
-    name='cart'
-    size={30}
-    color='black'
-    style={{ paddingRight: 15 }}
-    onPress={() => {navigation.navigate('Cart', { modalVisible: true })}}
-  />
-)
-}
+  const { items, updateItems } = React.useContext(Context);
+  return (
+    <View>
+      <Icon
+        name='cart'
+        size={30}
+        color='black'
+        style={styles.carrito}
+        onPress={() => {
+          navigation.navigate('Cart', { modalVisible: true });
+        }}
+      />
+      <View style={styles.notiContainer}>
+        <Text style={styles.noti}>{items.length}</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    
+  }, 
+  carrito: { paddingRight: 15 },
+  noti: {
+    backgroundColor: 'red',
+    width: 10,
+    color: 'white',
+    fontSize: 12,
+    borderRadius: 500,
+    
+  },
+  notiContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    position: 'absolute',
+    marginLeft: 20,
+    marginBottom: 15
+  }
+})
